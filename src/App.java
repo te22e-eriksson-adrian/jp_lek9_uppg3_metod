c4c240a973b06d8244ebcfbfb57657621ec86bb2
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-12-06 11:01:03 +0100

    inte klar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] osorterat = {6,3,5,4,1};
+        int[] sorterat = fält_sort(osorterat);
+        System.out.println(sorterat[0]+"\n"+sorterat[1]+"\n"+sorterat[2]+"\n"+sorterat[3]+"\n"+sorterat[4]+"\n"+sorterat[5]+"\n"+sorterat[6]+"\n"+sorterat[7]);
+    }
+    static int[] fält_sort(int falt[]){
+        
     }
 }
